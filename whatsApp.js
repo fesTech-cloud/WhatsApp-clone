4f5a70029053dc010ecd0e6e508bec0469210106
festus coder <festusishioma123@gmail.com>
2022-02-04 11:57:37 +0100

    updated whatsApp clone

--- whatsApp.js
+++ whatsApp.js
@@ -4,3 +4,83 @@
 // toggleButton.addEventListener("click", () => {
 //   menu.classList.toggle("header");
 // });
+
+/* features grid 1 */
+
+// .grid1-features {
+//   display: flex;
+// }
+
+// /* features grid1-span1 */
+// .grid1-features-span1 {
+//   padding-left: 9em;
+//   padding-right: 9em;
+//   text-align: center;
+//   background-color: #fcf2c6;
+//   justify-content: center;
+//   margin-left: 3em;
+//   margin-right: 1em;
+// }
+
+// .grid1-features-span1 h5 {
+//   color: #5e5e5e;
+//   font-size: 12px;
+//   padding-bottom: 10px;
+//   margin-top: 12vh;
+// }
+
+// .grid1-features-span1 h3 {
+//   padding-bottom: 25px;
+//   letter-spacing: 1.5px;
+//   font-weight: 300;
+//   font-size: 3.8vh;
+// }
+
+// .grid1-features-span1 h6 {
+//   color: #5e5e5e;
+//   font-weight: 400;
+//   font-size: 12px;
+//   line-height: 18px;
+//   padding-bottom: 25px;
+//   padding-top: 15px;
+// }
+
+// .grid1-features-span1 img {
+//   width: 18em;
+// }
+
+// /* features-grid1-span2 */
+// .grid1-features-span2 {
+//   text-align: center;
+//   background-color: #e4f3e3;
+//   margin-bottom: 50vh;
+//   margin-right: 3em;
+//   padding-left: 4em;
+//   padding-right: 5em;
+// }
+// .grid1-features-span2 img {
+//   width: 50vh;
+// }
+
+// .grid1-features-span2 h5 {
+//   color: #5e5e5e;
+//   font-size: 12px;
+//   padding-bottom: 10px;
+//   margin-top: 12vh;
+// }
+
+// .grid1-features-span2 h3 {
+//   padding-bottom: 25px;
+//   letter-spacing: 1.5px;
+//   font-weight: 300;
+//   font-size: 3.8vh;
+// }
+
+// .grid1-features-span2 h6 {
+//   color: #5e5e5e;
+//   font-weight: 200;
+//   font-size: 12px;
+//   line-height: 18px;
+//   padding-bottom: 25px;
+//   padding-top: 15px;
+// }
